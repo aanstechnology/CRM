2098ccae7f43b1ad0bd6120d67b1341a6e7e9542
Naveen Krishnappa Jadar(UST <u59132@ust-global.com>
2021-05-07 20:23:06 +0530

    welcome note is changed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to AANS Technologies</h1>
+      <h1>Welcome to AANS Technologies !!!!!! Hello world  #####  </h1>
     </div>
   );
 }
